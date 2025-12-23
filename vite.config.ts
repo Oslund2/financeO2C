5fceba8b85482e44303e7addff628b1623ab4eff
Kerry Oslund <91152356+Oslund2@users.noreply.github.com>
2025-12-22 21:44:24 -0500

    Updated tsconfig.node.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-});
+    exclude: ['lucide-react']
+  }
+})
